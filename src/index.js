1d3ce7c41fca236c1a56864118f0642add14ce0e
Lucius <luciushehe@protonmail.com>
2022-02-12 18:03:46 -0600

    nest invoice and expenses, add outlet to app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Invoices from "./routes/invoices";
+import Expenses from "./routes/expenses";
 import App from "./App";
 
 let rootElement = document.getElementById("root");
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/expenses" element={<Expenses />} />
+      </Route>
+    </Routes>
   </BrowserRouter>,
   rootElement);
